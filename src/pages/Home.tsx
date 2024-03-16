edaffbb36776064a4a29569c079b957e4f9193af
Aisultan Abdykerov <abdykerovaisultan@gmail.com>
2024-03-16 17:57:02 +0500

    feat: add BarChart component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { CompanyInfo } from '../components/CompanyInfo/CompanyInfo.tsx';
 import { Header } from '../components/header/Header.tsx';
 
+
 export const Home = () => {
   return (
     <>
