397c3c4f8f7f430198c1b8b5dd9856a124563867
Alexandr <rednaxela659@gmail.com>
2024-03-17 14:38:51 +0500

    refactor: change path to Header component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,11 @@
 import { Dashboard } from '../../components/Dashboard/Dashboard';
-import { Header } from '../../components/header/Header';
-
+import { Header } from '../../components/Header/Header';
 
 export const Home = () => {
   return (
     <>
       <Header />
-      <Dashboard/>
+      <Dashboard />
     </>
   );
 };
