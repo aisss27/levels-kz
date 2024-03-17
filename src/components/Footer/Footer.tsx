a02b810b1bd6c815d098f5828d4a6f1ed070482f
Aisultan Abdykerov <abdykerovaisultan@gmail.com>
2024-03-17 16:28:55 +0500

    style: changed photo positioning, extracted data outside of component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,7 @@
 import styles from './Footer.module.css';
-import aisavatar from '../../asssets/photos/aisphoto.jpeg';
+import { developers } from './data.ts';
 
 export const Footer = () => {
-  const developers = [
-    {
-      name: 'Aisultan',
-      githubUsername: 'aisss27',
-      avatarUrl: `${aisavatar}`,
-    },
-    {
-      name: 'Alexandr',
-      githubUsername: 'Sanechek1337',
-      avatarUrl: 'https://avatars.githubusercontent.com/u/60388678?v=4',
-    },
-    {
-      name: 'Arslan',
-      githubUsername: 'barbarian10',
-      avatarUrl: 'https://avatars.githubusercontent.com/u/3?v=4',
-    },
-  ];
-
   return (
     <footer className={styles.footer}>
       <div className={styles.developerContainer}>
