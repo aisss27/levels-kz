11346e30eabbb526eeb24a65dd33f1169fad6aca
Aisultan Abdykerov <abdykerovaisultan@gmail.com>
2024-03-18 20:58:01 +0500

    feat: add popular companies modal

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,5 +1,5 @@
 import Grid from '@mui/material/Unstable_Grid2';
-import { LatestOrders } from './LatestOrders.tsx';
+import { MyCompanies } from './MyCompanies.tsx';
 import { PopularCompanies } from './PopularCompanies.tsx';
 import { Salaries } from './Salaries.tsx';
 import { Traffic } from './Traffic.tsx';
@@ -9,25 +9,25 @@ import { HomeInfo } from '../CompanyInfo/HomeInfo.tsx';
 export function Dashboard() {
   return (
     <>
-    <HomeInfo/>
-    <Grid container spacing={3} sx={{padding: '20px'}}>
-      <Grid lg={6} md={12} xs={12}>
-        <Salaries sx={{ height: '100%' }} />
+      <HomeInfo />
+      <Grid container spacing={3} sx={{ padding: '20px' }}>
+        <Grid lg={6} md={12} xs={12}>
+          <Salaries sx={{ height: '100%' }} />
+        </Grid>
+        <Grid lg={6} md={12} xs={12}>
+          <Traffic
+            chartSeries={[40, 40, 20]}
+            labels={['Frontend', 'Backend', 'UI/UX']}
+            sx={{ height: '100%' }}
+          />
+        </Grid>
+        <Grid lg={6} md={12} xs={12}>
+          <PopularCompanies products={products} sx={{ height: '100%' }} />
+        </Grid>
+        <Grid lg={6} md={12} xs={12}>
+          <MyCompanies orders={orders} sx={{ height: '100%' }} />
+        </Grid>
       </Grid>
-      <Grid lg={6} md={12} xs={12}>
-        <Traffic
-          chartSeries={[40, 40, 20]}
-          labels={['Frontend', 'Backend', 'UI/UX']}
-          sx={{ height: '100%' }}
-        />
-      </Grid>
-      <Grid lg={6} md={12} xs={12}>
-        <PopularCompanies products={products} sx={{ height: '100%' }} />
-      </Grid>
-      <Grid lg={6} md={12} xs={12}>
-        <LatestOrders orders={orders} sx={{ height: '100%' }} />
-      </Grid>
-    </Grid>
     </>
   );
 }
--- src/components/Dashboard/PopularCompanies.tsx
+++ src/components/Dashboard/PopularCompanies.tsx
@@ -11,6 +11,8 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 import type { SxProps } from '@mui/material/styles';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Modal from '@mui/material/Modal';
+import styles from './PopularCompanies.module.css';
 
 export interface Product {
   id: string;
@@ -27,47 +29,98 @@ export function PopularCompanies({
   products = [],
   sx,
 }: LatestProductsProps): React.JSX.Element {
+  const [open, setOpen] = React.useState(false);
   return (
     <Card sx={sx}>
       <CardHeader title="Popular Companies" />
       <Divider />
       <List>
-        {products.map((product, index) => (
-          <ListItem divider={index < products.length - 1} key={product.id}>
-            <ListItemAvatar>
-              {product.image ? (
-                <Box
-                  component="img"
-                  src={product.image}
-                  sx={{ borderRadius: 1, height: '48px', width: '48px' }}
-                />
-              ) : (
-                <Box
-                  sx={{
-                    borderRadius: 1,
-                    backgroundColor: 'var(--mui-palette-neutral-200)',
-                    height: '48px',
-                    width: '48px',
-                  }}
-                />
-              )}
-            </ListItemAvatar>
-            <ListItemText
-              primary={product.name}
-              primaryTypographyProps={{ variant: 'subtitle1' }}
-              secondaryTypographyProps={{ variant: 'body2' }}
-            />
-            <Button variant="contained" endIcon={<ArrowForwardIcon />}>
-              explore
-            </Button>
-          </ListItem>
-        ))}
+        {products.slice(0, 5).map(
+          (
+            product,
+            index // Slice the array to show only the first 5 items
+          ) => (
+            <ListItem divider={index < 4} key={product.id}>
+              {' '}
+              {/* Add a divider for all items except the last one */}
+              <ListItemAvatar>
+                {product.image ? (
+                  <Box
+                    component="img"
+                    src={product.image}
+                    sx={{ borderRadius: 1, height: '48px', width: '48px' }}
+                  />
+                ) : (
+                  <Box
+                    sx={{
+                      borderRadius: 1,
+                      backgroundColor: 'var(--mui-palette-neutral-200)',
+                      height: '48px',
+                      width: '48px',
+                    }}
+                  />
+                )}
+              </ListItemAvatar>
+              <ListItemText
+                primary={product.name}
+                primaryTypographyProps={{ variant: 'subtitle1' }}
+                secondaryTypographyProps={{ variant: 'body2' }}
+              />
+              <Button variant="contained" endIcon={<ArrowForwardIcon />}>
+                explore
+              </Button>
+            </ListItem>
+          )
+        )}
       </List>
       <Divider />
       <CardActions sx={{ justifyContent: 'center' }}>
-        <Button color="inherit" size="large" variant="text">
+        <Button
+          onClick={() => setOpen(true)}
+          color="inherit"
+          size="large"
+          variant="text"
+        >
           View all
         </Button>
+        <Modal
+          open={open}
+          onClose={() => setOpen(false)}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box className={styles.modalBox}>
+            <table className={styles.table}>
+              <thead className={styles.table_heading}>Popular Companies</thead>
+              <tbody>
+                {products.map((product) => (
+                  <tr key={product.id}>
+                    <td>
+                      <div className={styles.companyLogoName}>
+                        <Box
+                          component="img"
+                          src={product.image}
+                          sx={{
+                            borderRadius: 1,
+                            height: '48px',
+                            width: '48px',
+                          }}
+                        />
+                        {product.name}
+                      </div>
+                      <Button
+                        variant="contained"
+                        endIcon={<ArrowForwardIcon />}
+                      >
+                        explore
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </Box>
+        </Modal>
       </CardActions>
     </Card>
   );
