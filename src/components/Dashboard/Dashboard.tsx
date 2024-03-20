fa76b8f321f58e6121a085c2cd9db2ef28b09c87
Alexandr <rednaxela659@gmail.com>
2024-03-21 00:32:25 +0500

    feat: create variable for sx styles

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -6,24 +6,31 @@ import { Traffic } from './Traffic.tsx';
 import { orders, companies } from './data.ts';
 
 export function Dashboard() {
+  const cardStyles = {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
+
   return (
     <>
       <Grid container spacing={3} sx={{ padding: '20px' }}>
         <Grid item lg={6} md={12} xs={12}>
-          <Salaries sx={{ height: '100%' }} />
+          <Salaries sx={cardStyles} />
         </Grid>
         <Grid item lg={6} md={12} xs={12}>
           <Traffic
             chartSeries={[40, 40, 20]}
             labels={['Frontend', 'Backend', 'UI/UX']}
-            sx={{ height: '100%' }}
+            sx={cardStyles}
           />
         </Grid>
         <Grid item lg={6} md={12} xs={12}>
-          <PopularCompanies companies={companies} sx={{ height: '100%' }} />
+          <PopularCompanies companies={companies} sx={cardStyles} />
         </Grid>
         <Grid item lg={6} md={12} xs={12}>
-          <MyCompanies orders={orders} sx={{ height: '100%' }} />
+          <MyCompanies orders={orders} sx={cardStyles} />
         </Grid>
       </Grid>
     </>
