078b4829260cc7dc0b9b894631e51266e8dd1dd4
Alexandr <rednaxela659@gmail.com>
2024-03-20 18:07:13 +0500

    feat: delete company and companies pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,8 +2,6 @@ import { createBrowserRouter } from 'react-router-dom';
 import Form from './components/AddSalaryPage/Form.tsx';
 import { InitialValues } from './components/AddSalaryPage/InitialValues.ts';
 import { Home } from './pages/Home/Home.tsx';
-import { CompanyPage } from './pages/CompanyPage/CompanyPage.tsx';
-import { CompaniesPage } from './pages/AllCompaniesPage/CompaniesPage.tsx';
 
 export const router = createBrowserRouter([
   {
@@ -13,13 +11,5 @@ export const router = createBrowserRouter([
   {
     path: '/addsalary',
     element: <Form initialValues={InitialValues} />,
-  },
-  {
-    path: '/submittedsalaries',
-    element: <CompanyPage />,
-  },
-  {
-    path: '/companies',
-    element: <CompaniesPage />,
   },
 ]);
