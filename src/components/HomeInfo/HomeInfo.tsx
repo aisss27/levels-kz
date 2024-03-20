de8a4ec98d0a49bcb29dbfb99866b90ea9b0e97f
Aisultan Abdykerov <abdykerovaisultan@gmail.com>
2024-03-20 16:34:27 +0500

    style: rename and add classes to elements in css

--- src/components/HomeInfo/HomeInfo.tsx
+++ src/components/HomeInfo/HomeInfo.tsx
@@ -8,16 +8,16 @@ export const HomeInfo = () => {
       </h2>
       <div className={styles.card}>
         <h3 className={styles.subHeading}>Факторы, влияющие на зарплаты</h3>
-        <p>
+        <p className={styles.text}>
           Несколько ключевых факторов, влияющих на уровень заработной платы <br/>
           в IT-сфере Казахстана:
         </p>
-        <ul>
-          <li>Опыт работы</li>
-          <li>Образование и сертификации</li>
-          <li>Специализация и навыки</li>
-          <li>Регион работы</li>
-          <li>Текущие рыночные тренды и спрос</li>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Опыт работы</li>
+          <li className={styles.listItem}>Образование и сертификации</li>
+          <li className={styles.listItem}>Специализация и навыки</li>
+          <li className={styles.listItem}>Регион работы</li>
+          <li className={styles.listItem}>Текущие рыночные тренды и спрос</li>
         </ul>
       </div>
     </div>
