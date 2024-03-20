e602bee1888739e80e3ee5a20cc892fd9769945a
Alexandr <rednaxela659@gmail.com>
2024-03-20 11:48:46 +0500

    feat: create login page

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import axios from 'axios';
+import styles from './LoginPage.module.css';
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    axios.post('https://onelab-levels-api.vercel.app/api/auth/signin', {
+      email: email,
+      password: password,
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h2 className={styles.loginLabel}>Вход в аккаунт</h2>
+        <div className={styles.loginForm}>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className={styles.input}
+            type="email"
+            name="email"
+            placeholder="Логин"
+            required
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className={styles.input}
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            required
+          />
+          <button
+            className={styles.loginButton}
+            onClick={handleLogin}
+            type="submit"
+          >
+            Войти
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
