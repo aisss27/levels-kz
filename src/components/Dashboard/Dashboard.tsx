27007368a9cd56dc4320b69ceeb227560c954cfe
Alexandr <rednaxela659@gmail.com>
2024-03-20 22:43:15 +0500

    fix: change grid import (fix error)

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import Grid from '@mui/material/Unstable_Grid2';
+import { Grid } from '@mui/material';
 import { MyCompanies } from './MyCompanies.tsx';
 import { PopularCompanies } from './PopularCompanies.tsx';
 import { Salaries } from './Salaries.tsx';
@@ -12,20 +12,20 @@ export function Dashboard() {
     <>
       <HomeInfo />
       <Grid container spacing={3} sx={{ padding: '20px' }}>
-        <Grid lg={6} md={12} xs={12}>
+        <Grid item lg={6} md={12} xs={12}>
           <Salaries sx={{ height: '100%' }} />
         </Grid>
-        <Grid lg={6} md={12} xs={12}>
+        <Grid item lg={6} md={12} xs={12}>
           <Traffic
             chartSeries={[40, 40, 20]}
             labels={['Frontend', 'Backend', 'UI/UX']}
             sx={{ height: '100%' }}
           />
         </Grid>
-        <Grid lg={6} md={12} xs={12}>
+        <Grid item lg={6} md={12} xs={12}>
           <PopularCompanies companies={companies} sx={{ height: '100%' }} />
         </Grid>
-        <Grid lg={6} md={12} xs={12}>
+        <Grid item lg={6} md={12} xs={12}>
           <MyCompanies orders={orders} sx={{ height: '100%' }} />
         </Grid>
       </Grid>
