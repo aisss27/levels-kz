bfe84fd6afe1d3804f21138b79e4468dd654babd
Alexandr <rednaxela659@gmail.com>
2024-03-21 14:06:07 +0500

    feat: add auth reducer to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import comparasionReducer from './slices/comparasionSlice';
 import companiesListReducer from './slices/companiesListSlice';
+import authReducer from './slices/authSlice.ts';
 
 export const store = configureStore({
   reducer: {
     comparasionReducer,
     companiesListReducer,
+    authReducer,
   },
 });
 
